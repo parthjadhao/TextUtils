295ae8c6337c20e065af28a9e95fee712842cd73
Parth <84613656+parthjadhao@users.noreply.github.com>
2023-11-12 16:46:06 +0530

    video-13:Added auto-dimising alert message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+// Imports
+import React, { useState } from "react";
 import "./App.css";
+// component Imports
 // import AboutUs from "./components/AboutUs";
+import ImpAlert from "./components/ImpAlert";
 import Navbar from "./components/navbar";
 import TextForm from "./components/TextForm";
-import React, { useState } from "react";
 function App() {
+  // states
   const [mode, setMode] = useState("light");
+  const [alert, setalert] = useState(null);
+
+  // fuctions
   const toggleMode = () => {
     if (mode === "light") {
       // console.log("whtie = dark mode = " + mode);
-      document.body.style.background = "black"
       setMode("dark");
+      document.body.style.background = "black";
+      showAlert("dark Mode enabled", "success");
     } else {
       // console.log("dark = white mode = " + mode);
       setMode("light");
-      document.body.style.background = "white"
+      document.body.style.background = "white";
+      showAlert("Light Mode enabled", "success");
     }
   };
+  const showAlert = (message, type) => {
+    setalert({
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setalert(null);
+    }, 1500);
+  };
+
   return (
     <>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <ImpAlert alert={alert} />
       <TextForm heading="Enter text to analyze below" mode={mode} />
       {/* <AboutUs/> */}
     </>
